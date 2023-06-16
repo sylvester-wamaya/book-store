d3d742901628608da4118604faf20e72d34da5c7
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-17 01:03:47 +0300

    Add a list of books

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import BookCard from './BookCard'
 
 function Home() {
+    const books = [
+        {id: 1, title: "The secret", author: "Rhonda Byrne", category: "Self-help"},
+        {id: 2, title: "Into the Wild", author: "Jon Krakauer", category: "Adventure"},
+        {id: 3, title: "Treasure Island", author: "Robert Louis Stevenson", category: "Action"},
+        {id: 4, title: "American Gods", author: "Neil Gaiman", category: "Fantasy"},
+]
   return (
     <div>
-      Home page
+      <h1>Home</h1>
+      <ul>
+      {
+        books.map((book)=>(
+            <BookCard title={book.title} author={book.author} category={book.category} key={book.id}/>
+        ))
+      }
+      </ul>
     </div>
   )
 }
