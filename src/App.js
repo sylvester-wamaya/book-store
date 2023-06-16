e835049e9716fa4fd31e56b0dc57fe855ad3cb98
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-16 23:16:25 +0300

    Render navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Categories from './components/Categories';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
+    <>
+    <NavBar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='categories' element={<Categories/>}/>
     </Routes>
+    </>
   );
 }
 
