0b6f2a79db8e8425a4f978130ef550a29533aca5
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-20 23:00:29 +0300

    Add categories slice

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    categories: [],
+}
+
+const categoriesSlice = createSlice({
+    name:'categories',
+    initialState,
+    reducers:{
+        
+    }
+})
