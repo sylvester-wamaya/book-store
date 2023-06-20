69c9addfeeb780b307d48d14caecdbbedf12939c
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-20 22:00:39 +0300

    Implemenent addbook to the the Addbook component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
 function AddBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
   return (
     <>
       <h2>ADD NEW BOOK</h2>
       <form>
-        <input name="book" type="text" placeholder="Book Title" />
-        <input name="book" type="text" placeholder="Book Author" />
-        <button type="submit">ADD BOOK</button>
+        <input onChange={(e) => setTitle(e.target.value)} name="title" type="text" placeholder="Book Title" />
+        <input onChange={(e) => setAuthor(e.target.value)} name="author" type="text" placeholder="Book Author" />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch(addBook({ title, author }));
+          }}
+          type="submit"
+        >
+          ADD BOOK
+        </button>
       </form>
     </>
   );
