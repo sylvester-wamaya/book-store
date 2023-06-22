f8006d9216f5658b458fbfc229e0b098db71632c
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-22 22:53:00 +0300

    Clear the inputs after form submit

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -4,26 +4,28 @@ import { useDispatch } from 'react-redux';
 
 import { addBooks } from '../redux/books/booksSlice';
 
+
 function AddBook() {
   const [book, setBook] = useState({title: '', author: '', category: "Fiction"});
   const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
   
-
+const handleSubmit = (e) => {
+  e.preventDefault();
+  dispatch(addBooks({
+    book,
+  }))
+  setBook({title: '', author: ''})
+}
 
   return (
     <>
       <h2>ADD NEW BOOK</h2>
-      <form>
-        <input onChange={(e) => setBook({...book, title: e.target.value})} name="title" type="text" placeholder="Book Title" required />
-        <input onChange={(e) => setBook({...book, author: e.target.value})} name="author" type="text" placeholder="Book Author" required />
+      <form onSubmit={handleSubmit}>
+        <input value={book.title} onChange={(e) => setBook({...book, title: e.target.value})} name="title" type="text" placeholder="Book Title" required />
+        <input value={book.author} onChange={(e) => setBook({...book, author: e.target.value})} name="author" type="text" placeholder="Book Author" required />
         <button
-          onClick={(e) => {
-            e.preventDefault();
-            dispatch(addBooks({
-              book,
-            }));
-          }}
+         
           type="submit"
         >
           ADD BOOK
