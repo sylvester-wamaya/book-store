20aa3a94cdd3acc256195f9b89e04b81eaa5d7bd
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-23 21:45:46 +0300

    Style the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,23 @@
+/* eslint-disable */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import {BiSolidUser} from 'react-icons/bi'
+import styles from '../styles/Navbar.module.css'
 
 function NavBar() {
+ 
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="categories">Categories</NavLink>
+    <nav>
+    <div className={styles.navDiv}>
+      <h1>Bookstore CMS</h1>
+      <NavLink  className={styles.navHome} to="/">BOOKS</NavLink>
+      <NavLink  className={styles.navCategories} to="categories">CATEGORIES</NavLink>
     </div>
+    <span className={styles.navUser}>
+    <BiSolidUser/>
+    </span>
+    </nav>
+
   );
 }
 
