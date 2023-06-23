b63565615f7848e80fb8426a94fd1961acd51c4f
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-23 22:05:48 +0300

    Add content to the bookcard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,7 +1,9 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteBook } from '../redux/books/booksSlice';
+import styles from '../styles/BookCard.module.css'
 
 function BookCard({
   id, title, author, category,
@@ -17,16 +19,23 @@ function BookCard({
   return (
 
     <li>
-      <p>{category}</p>
-      <br />
-      <span><b>{title}</b></span>
-      <br />
-      <span>{author}</span>
-      <br />
-      <div>
+      <div className={styles.book }>
+      <p className={styles.category}>{category}</p>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.author}>{author}</p>
+      <div className={styles.bookBtns}>
         <button type="button">Comments</button>
         <button onClick={() => dispatch(deleteBook(id))} type="button">Remove</button>
         <button type="button">Edit</button>
+      </div>
+      </div>
+      <div>
+        <div className={styles.progress}></div>
+      </div>
+      <div>
+        <p>Current Chapter</p>
+        <p>Introduction</p>
+        <button type='button'>Update progress</button>
       </div>
     </li>
 
