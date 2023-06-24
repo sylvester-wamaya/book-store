e43148507d5292b5b7ec75de5a64ff7b13477dbd
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-24 15:39:37 +0300

    Add classNames and styling to the components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,19 @@
-/* eslint-disable */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import {BiSolidUser} from 'react-icons/bi'
-import styles from '../styles/Navbar.module.css'
+import { BiSolidUser } from 'react-icons/bi';
+import styles from '../styles/Navbar.module.css';
 
 function NavBar() {
- 
   return (
     <nav>
-    <div className={styles.navDiv}>
-      <h1>Bookstore CMS</h1>
-      <NavLink  className={styles.navHome} to="/">BOOKS</NavLink>
-      <NavLink  className={styles.navCategories} to="categories">CATEGORIES</NavLink>
-    </div>
-    <span className={styles.navUser}>
-    <BiSolidUser/>
-    </span>
+      <div className={styles.navDiv}>
+        <h1>Bookstore CMS</h1>
+        <NavLink className={styles.navHome} to="/">BOOKS</NavLink>
+        <NavLink className={styles.navCategories} to="categories">CATEGORIES</NavLink>
+      </div>
+      <span className={styles.navUser}>
+        <BiSolidUser />
+      </span>
     </nav>
 
   );
